636454fe2193bbfb04c0e9eb39709c03bad67031
左丘颖逸 <49649786+Zuoqiu-Yingyi@users.noreply.github.com>
2019-12-19 17:22:17 +0800

    v1.5.1
    改bug

--- digital_tube_display_selector.v
+++ digital_tube_display_selector.v
@@ -4,9 +4,9 @@ module digital_tube_display_selector(
    input [7:0] codeout0,   //数码管信号0输入
    input [7:0] codeout1,   //数码管信号1输入
    input [7:0] codeout2,   //数码管信号2输入
-   input seg0,    //位选信号0输入
-   input seg1,    //位选信号1输入
-   input seg2,    //位选信号2输入
+   input [7:0] seg0,    //位选信号0输入
+   input [7:0] seg1,    //位选信号1输入
+   input [7:0] seg2,    //位选信号2输入
    output reg [7:0] codeout, //数码管最终控制信号
    output reg [7:0] seg      //位选信号
 );
